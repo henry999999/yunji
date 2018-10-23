55c024aafbe16766bb806a537f623265513a86b8
HL <HL@qq.com>
2018-10-23 22:30:57 +0800

    完成搜索页

--- public/yj/js/search.js
+++ public/yj/js/search.js
@@ -2,5 +2,22 @@
  * Created by Administrator on 2018/10/23 0023.
  */
 $(function(){
-
+    var keyarr=[];
+    $(".search-btn").on("tap",function(){
+        //location.href
+        var keyword=$(".search-text").val();
+        if(keyword){
+            keyarr.unshift(keyword);
+            localStorage.setItem("keyArr",JSON.stringify(keyarr));
+            location.href="search-list.html?keyword="+keyword;
+            $(".search-text").val("");
+        }else{
+            alert("ÇëÊäÈëËÑË÷¹Ø¼ü×Ö");
+        }
+    })
+    var data=JSON.parse(localStorage.getItem("keyArr"));
+    console.log(data);
+    var serhtml=template("history",{data:data});
+    $(".history-txt").html(serhtml);
+    console.log(serhtml);
 })
